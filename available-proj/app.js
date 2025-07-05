1087782fe740636f8ad7bc9b1d34173ce11b25bf
timebe00 <an0160@naver.com>
2025-07-05 14:37:32 +0900

    app 정보 세팅

--- available-proj/app.js
+++ available-proj/app.js
@@ -9,33 +9,51 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+setApp = () => {
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+  return new Promise(
+    async (resolve, reject) => {
+      try {
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+        global._base = __dirname;
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+        // view engine setup
+        app.set('views', path.join(__dirname, 'views'));
+        app.set('view engine', 'ejs');
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+        app.use(logger('dev'));
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: false }));
+        app.use(cookieParser());
+        app.use(express.static(path.join(__dirname, 'public')));
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+        app.use('/', indexRouter);
+        app.use('/users', usersRouter);
 
-module.exports = app;
+        // catch 404 and forward to error handler
+        app.use(function (req, res, next) {
+          next(createError(404));
+        });
+
+        // error handler
+        app.use(function (err, req, res, next) {
+          // set locals, only providing error in development
+          res.locals.message = err.message;
+          res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+          // render the error page
+          res.status(err.status || 500);
+          res.render('error');
+        });
+
+        resolve(app);
+
+      } catch (error) {
+        console.log("error : ", error)
+        reject(error);
+      }
+    }
+  )
+};
+
+module.exports = setApp();
