96884dde79d87f9e0a77ceab1e43b43cac4c560f
timebe00 <an0160@naver.com>
2025-08-06 19:56:34 +0900

    에디터 파일 업로드로 변경

--- available-proj/api/routes/index.js
+++ available-proj/api/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const multer = require('multer');
+const path = require('path');
+
 const indexService = require("../service/index_service");
 
 router.get('/', async function (req, res, next) {
@@ -12,5 +15,21 @@ router.get('/', async function (req, res, next) {
   }
 
 });
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads/editor/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '_' + file.originalname);
+  }
+});
+const upload = multer({ storage });
+
+router.post('/upload/image', upload.single('file'), (req, res) => {
+  const fileUrl = `/uploads/editor/${req.file.filename}`;
+  res.json({ url: fileUrl });
+});
+
 
 module.exports = router;
